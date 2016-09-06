0448e2bed9e25fd918083ea2d5f0d5d58186258a
xiuno <xiuno@xiuno.com>
2016-09-06 13:30:37 +0800

    xiuno基础版“

---
+++ lang/zh-cn/bbs.js
@@ -0,0 +1,22 @@
+var lang = {
+	'warning': '警告',
+	'tips_title': '提示：',
+	'confirm': '确定',
+	'confirm_title': '请确认以下信息：',
+	'close': '关闭',
+	'yes': '是',
+	'no': '否',
+	
+	'please_choose_thread':'请选择主题',
+	'move_forum':'移动版块',
+	'choose_move_forum':'选择移动的版块',
+	'top_0':'取消置顶',
+	'top_1':'版块置顶',
+	'top_3':'全站置顶',
+	'top_thread':'置顶主题',
+	'top_range':'置顶范围',
+	'confirm_delete_thread':'确定删除选中的 ({n}) 篇主题吗？',
+	'confirm_delete':'确定删除吗？',
+	
+	// hook lang_zh_cn_bbs_js.htm
+};
